7f5eda929cf025a0ec982d23795bb596f9a635f9
Mark Jan Jacobi <mark@jacobi.pm>
2015-02-09 18:47:23 +0100

    Unpacked file, and started on heap

---
+++ G1/heap/heap.c
@@ -0,0 +1,31 @@
+#include <assert.h>
+#include "heap.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+  printf("Test print\n");
+  heap s;
+  heap_initialize(&s)
+
+}
+
+void heap_initialize(heap* heap_p){
+
+}
+
+void heap_clear(heap*){
+
+}
+size_t heap_size(heap*){
+
+}
+void* heap_top(heap*){
+
+}
+void heap_insert(heap*, void*, int){
+
+}
+void *heap_pop(heap*){
+
+}
