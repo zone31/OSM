d2c23b75be2ece4d35fa091dc0c4a3f24cff16e0
Arkimedes af Syrakus <arkimedes@osm15>
2015-02-21 15:52:49 +0100

    fixed unused arguments

--- G2/buenos-g2/tests/process_spawn.c
+++ G2/buenos-g2/tests/process_spawn.c
@@ -1,9 +1,10 @@
 #include "process_spawn.h"
 #include "proc/process.h"
+#include "kernel/process_control.h"
 
 const char *read_write = "[disk]rw";
 
-int main(int argc, char const *argv[])
+int main()
 {
     syscall_exec(read_write);
 
