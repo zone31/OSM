592183f1c0380672d7ff4a1912db33bbdc30424a
bus000 <magnus@stavngaard.dk>
2015-02-23 09:48:36 +0100

    Added IO to module file

--- G2/buenos-g2/kernel/module.mk
+++ G2/buenos-g2/kernel/module.mk
@@ -6,7 +6,7 @@ MODULE := kernel
 
 FILES := cswitch.S panic.c kmalloc.c interrupt.c thread.c \
          scheduler.c _interrupt.S _spinlock.S idle.S sleepq.c semaphore.c \
-         exception.c halt.c process_control.c
+         exception.c halt.c process_control.c IO.c
 
 SRC += $(patsubst %, $(MODULE)/%, $(FILES))
 
