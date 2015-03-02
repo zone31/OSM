4696e294b680d2ec4091c635f64c0af7cc19266c
bus000 <magnus@stavngaard.dk>
2015-03-02 13:56:12 +0100

    Made the length of a semaphore name a fixed size.

--- G3/buenos-g3/proc/semaphore.h
+++ G3/buenos-g3/proc/semaphore.h
@@ -14,8 +14,8 @@ typedef enum {
 typedef struct {
     semaphore_t *kernel_semaphore;
     semstatus status;
-    const char *name;
-    int self;
+    char name[SEM_NAME_MAX_LENGTH];
+    int ident;
 } usr_sem_t_wrapper;
 
 typedef void usr_sem_t;
@@ -23,13 +23,10 @@ typedef void usr_sem_t;
 #define HANDLE_IS_NULL -1
 #define SUCCESS 0
 
-
 usr_sem_t * syscall_sem_open(char const *name, int value);
 int syscall_sem_p(usr_sem_t *handle);
 int syscall_sem_v(usr_sem_t *handle);
 int syscall_sem_destroy(usr_sem_t *handle);
 void sem_user_init(void);
-usr_sem_t * get_semaphore(char const *name);
-usr_sem_t *alloc_semaphore(char const *name);
 
 #endif
