6737f90ac43e31c14c76e23bc8bccc06f2d7471d
bus000 <magnus@stavngaard.dk>
2015-03-02 13:55:41 +0100

    Made procure and vacate work, hopefully

--- G3/buenos-g3/proc/semaphore.c
+++ G3/buenos-g3/proc/semaphore.c
@@ -1,5 +1,11 @@
 #include "kernel/semaphore.h"
 #include "semaphore.h"
+#include "kernel/kmalloc.h" /* REMOVE. */
+#include "kernel/assert.h"  /* REMOVE. */
+
+/* Helper functions. */
+usr_sem_t_wrapper * get_semaphore(char const *name);
+usr_sem_t_wrapper *alloc_semaphore(char const *name);
 
 /* An array where we store the userland semaphores. */
 usr_sem_t_wrapper semaphores[SEM_MAX_SEMAPHORES];
@@ -10,8 +16,8 @@ void sem_user_init(void)
 
     for (i = 0; i < SEM_MAX_SEMAPHORES; i++){
         semaphores[i].status = SEM_NONALLOCED;
-        semaphores[i].name = "";
-        semaphores[i].self = i;
+        semaphores[i].name[0] = '\0';
+        semaphores[i].ident = i;
     }
 }
 
@@ -31,12 +37,16 @@ usr_sem_t * syscall_sem_open(char const *name, int value)
 
     if (value >= 0) {
         /* Create semaphore with value.  If already exist return NULL. */
-        if ((new_sem = alloc_semaphore(name)) != NULL){
+        if ((new_sem = alloc_semaphore(name)) != NULL) {
             new_sem->kernel_semaphore = semaphore_create(value);
-            new_sem->name = name;
-            return new_sem;
-        }
-        else{
+            stringcopy(new_sem->name, name, SEM_NAME_MAX_LENGTH-1);
+
+            if (new_sem->kernel_semaphore == NULL) {
+                return NULL;
+            } else {
+                return &(new_sem->ident);
+            }
+        } else {
             return NULL;
         }
     } else {
@@ -44,25 +54,25 @@ usr_sem_t * syscall_sem_open(char const *name, int value)
     }
 }
 
-usr_sem_t *get_semaphore(char const *name){
+usr_sem_t_wrapper *get_semaphore(char const *name){
     int i;
 
     for (i = 0; i < SEM_MAX_SEMAPHORES; i++){
         if(stringcmp(name, semaphores[i].name) == 0){
-            return &semaphores[i].self;
+            return &semaphores[i];
         }
     }
     return NULL;
 }
 
-usr_sem_t *alloc_semaphore(char const *name){
+usr_sem_t_wrapper *alloc_semaphore(char const *name){
     int i;
 
     if (get_semaphore(name) == NULL){
         for (i = 0; i < SEM_MAX_SEMAPHORES; i++){
             if(semaphores[i].status == SEM_NONALLOCED){
                 semaphores[i].status = SEM_ALLOCED;
-                return (usr_sem_t *)&semaphores[i].self;
+                return (usr_sem_t *)&semaphores[i];
             }
         }
     }
@@ -72,22 +82,45 @@ usr_sem_t *alloc_semaphore(char const *name){
 int syscall_sem_destroy(usr_sem_t* handle){
     usr_sem_t_wrapper *handle_wrapper = &semaphores[*((int *)handle)];
     handle_wrapper->status = SEM_NONALLOCED;
+
+    /* Destroy kernel semaphore. */
+    semaphore_destroy(handle_wrapper->kernel_semaphore);
+
     return 0;
 }
 
 int syscall_sem_v(usr_sem_t* handle){
-    if (handle == NULL){ return HANDLE_IS_NULL; }
-    usr_sem_t_wrapper *handle_wrapper = &semaphores[*((int *)handle)];
+    int index;
+    usr_sem_t_wrapper *handle_wrapper;
 
+    /* Return error if handle is NULL. */
+    if (handle == NULL) {
+        return HANDLE_IS_NULL;
+    }
+
+    /* Get the index in the array. */
+    index = *((int *)handle);
+
+    /* Get the actual handle and vacate the resource. */
+    handle_wrapper = &(semaphores[index]);
     semaphore_V(handle_wrapper->kernel_semaphore);
 
     return 0;
 }
 
 int syscall_sem_p(usr_sem_t* handle){
-    if (handle == NULL){ return HANDLE_IS_NULL; }
+    int index;
+    usr_sem_t_wrapper *handle_wrapper;
 
-    usr_sem_t_wrapper *handle_wrapper = &semaphores[*((int *)handle)];
+    /* Return error if handle is NULL. */
+    if (handle == NULL) { return HANDLE_IS_NULL; }
+
+    /* Get the index in the array. */
+    index = *((int *) handle);
+
+    /* Get the actual handle and procure a resource. */
+    handle_wrapper = &(semaphores[index]);
     semaphore_P(handle_wrapper->kernel_semaphore);
+
     return 0;
 }
