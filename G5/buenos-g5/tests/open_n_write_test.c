27fe7a41ab86154ce1e390f1eb2202e5c023788e
bus000 <magnus@stavngaard.dk>
2015-03-10 17:15:41 +0100

    new test

---
+++ G5/buenos-g5/tests/open_n_write_test.c
@@ -0,0 +1,34 @@
+#include "tests/lib.h"
+#include "fs/vmf.h"
+
+int main()
+{
+    const char *filename = "[disk]Mark_is_fag";
+    int size = 1 << 3;
+    int ret_val;
+    int filehandle;
+
+    /* Create a file. */
+    ret_val = syscall_create(filename, size);
+
+    printf("create returned %d\n", ret_val);
+
+    /* Open the file. */
+    filehandle = retval = syscall_open(filename);
+
+    printf("open returned %d\n", ret_val);
+
+    /* Write to the file. */
+    ret_val = syscall_write(filehandle, (void *) "mark is fag", 100);
+
+    printf("write returned %d\n", ret_val);
+
+    /* Close the filehandle. */
+    ret_val = syscall_close(filehandle);
+
+    printf("close returned %d\n", ret_val);
+
+    syscall_exit(0);
+
+    return 0;
+}
